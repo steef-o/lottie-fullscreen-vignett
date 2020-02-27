696228d680fc2ab123150c41b7f50a809dfe4aa5
Steffen Andre Hagen <steffen.hagen@knowit.no>
2020-02-27 09:48:30 +0100

    added ability to set custom text

--- js/main.js
+++ js/main.js
@@ -8,8 +8,16 @@ const animation = lottie.loadAnimation({
     path: 'data/vignett_v.02.json'
 });
 
-animation.addEventListener('data_ready', function() {
+animation.addEventListener('DOMLoaded', function() {
     animation.addEventListener('loopComplete', function() {
         animContainer.style.display = 'none';
     });
+
+    //tspan element
+    const text = document.getElementById('loader-text').children[0].children[0];
+
+    // const setText = newText => {
+    //     text.innerHTML = newText;
+    // };
+    // setText('Fasit is back!');
 });
