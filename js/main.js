08273218ac3a339432b41929c39c069d28ac7d26
Steffen Andre Hagen <steffen.a.hagen@gmail.com>
2020-05-19 14:56:17 +0200

    Fixed issue with background not filling screen on all aspect ratios

--- js/main.js
+++ js/main.js
@@ -1,38 +1,53 @@
-
-
 const date = new Date();
-const weekday  = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+const weekday = new Array(7);
+weekday[0] = 'Sunday';
+weekday[1] = 'Monday';
+weekday[2] = 'Tuesday';
+weekday[3] = 'Wednesday';
+weekday[4] = 'Thursday';
+weekday[5] = 'Friday';
+weekday[6] = 'Saturday';
 
+if (weekday[date.getDay()] === 'Tuesday') {
+    const characterContainer = document.getElementById('character');
+    const backdropContainer = document.getElementById('backdrop');
 
-// if(weekday[date.getDay()] === "Friday") {
-    const animContainer = document.getElementById('loader');
-
-    const animation = lottie.loadAnimation({
-        container: animContainer,
+    const backdrop = lottie.loadAnimation({
+        container: backdropContainer,
         renderer: 'svg',
         autoplay: true,
-        loop: 1,
-        path: 'data/vignette_v.06.json'
+        loop: false,
+        path: 'data/backdrop_v.02.json',
+        rendererSettings: {
+            preserveAspectRatio: 'none'
+        }
     });
 
-    animation.addEventListener('DOMLoaded', function() {
-        animation.addEventListener('loopComplete', function() {
-            animContainer.style.display = 'none';
+    const character = lottie.loadAnimation({
+        container: characterContainer,
+        renderer: 'svg',
+        autoplay: true,
+        loop: false,
+        path: 'data/character_v.02.json',
+    });
+
+    backdrop.addEventListener('DOMLoaded', function() {
+        backdropContainer.style.width = '100vw';
+        backdropContainer.style.height = '100vh';
+
+        backdrop.playSegments([1, 240], true);
+        backdrop.addEventListener('complete', function() {
+            backdropContainer.style.display = 'none';
+            characterContainer.style.display = 'none';
         });
         // setCustomText("Fasit is back!")
     });
-
-// }
+}
 
 function setCustomText(text) {
     // Get tspan element
-    const textRef = document.getElementById('loader-text').getElementsByTagName("tspan")[0];
+    const textRef = document
+        .getElementById('loader-text')
+        .getElementsByTagName('tspan')[0];
     textRef.innerHTML = text;
 }
